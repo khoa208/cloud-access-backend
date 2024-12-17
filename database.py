9c303d9f7eb7b9a5bd37402bd1e073705faa6e41
khoa208 <khoadodev@gmail.com>
2024-12-17 15:36:35 -0800

    change db credential

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # PostgreSQL
-DATABASE_URL = "postgresql://username:password@localhost/cloud_access"
+DATABASE_URL = "postgresql://postgres:1234@localhost:5432/CloudAccess"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
