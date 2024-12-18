375f621218509b09a09fbb5a40514533d2c3d02c
khoa208 <khoadodev@gmail.com>
2024-12-17 20:42:41 -0800

    add comment

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# PostgreSQL
+# PostgreSQL "postgresql://username:password@localhost:5432/name of the database"
 DATABASE_URL = "postgresql://postgres:1234@localhost:5432/CloudAccess"
 
 engine = create_engine(DATABASE_URL)
